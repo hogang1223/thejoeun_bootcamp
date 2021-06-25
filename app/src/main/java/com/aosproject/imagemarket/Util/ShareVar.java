b21c53f1c82c44f0028da74f4d4561b019f08265
Lee-Dohee <ehgml991210@naver.com>
2021-06-25 17:37:09 +0900

    first commit

--- app/src/main/java/com/aosproject/imagemarket/Util/ShareVar.java
+++ app/src/main/java/com/aosproject/imagemarket/Util/ShareVar.java
@@ -3,5 +3,8 @@ package com.aosproject.imagemarket.Util;
 public class ShareVar {
     //public static final String macIP = "http:// 본인 아이피 주소  :8080/imagemarket/";
     public static final String macIP = "http://192.168.2.7:8080/imagemarket/";
+    public static final String hjIP = "";
+    public static final String hgIP = "";
+    public static final String dyIP = "";
     public static final String loginEmail = "buyuser@gmail.com";
 }
